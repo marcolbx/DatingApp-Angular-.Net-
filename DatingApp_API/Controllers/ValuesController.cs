e57987d37f708d0efb4c745fb27c5eac7d17bf02
marcolbx <marcolbx@hotmail.com>
2020-06-01 22:28:54 -0400

    Initial Commit

---
+++ DatingApp_API/Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    //https://localhost:44396/api/values
+    //http://localhost:5000/api/values
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(value);
+        }
+
+        //Post api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        //PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put([FromBody] string value)
+        {
+
+        }
+
+        //DELETE api/value/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+    }
+
+
+}
