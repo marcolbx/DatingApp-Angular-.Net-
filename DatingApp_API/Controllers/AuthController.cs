71700fb88d55fe59cd2e81a2c19024ba0439e100
marcolbx <marcolbx@hotmail.com>
2020-06-06 21:38:34 -0400

    Section 6. New template and notifications

--- DatingApp_API/Controllers/AuthController.cs
+++ DatingApp_API/Controllers/AuthController.cs
@@ -48,7 +48,6 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("NONOON");
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
