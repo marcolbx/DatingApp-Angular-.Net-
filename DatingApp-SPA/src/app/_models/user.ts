6a43f8a1c2adbe93185262bb52642e2e7265392c
marcolbx <marcolbx@hotmail.com>
2020-06-23 19:36:09 -0400

    Section 14 - Paging & Sorting

--- DatingApp-SPA/src/app/_models/user.ts
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -5,7 +5,7 @@ export interface User {
     username: string;
     knownAs: string;
     age: number;
-    Gender: string;
+    gender: string;
     created: Date;
     lastActive: Date;
     photoUrl: string;
